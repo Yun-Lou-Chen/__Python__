5898ac76a791ce4face3a08979ee2b2b95bfd653
YunLou <151415345+Yun-Lou-Chen@users.noreply.github.com>
2023-12-16 10:39:54 +0800

    Modily

--- led_pwm.py
+++ led_pwm.py
@@ -2,23 +2,25 @@
 from machine                import Pin,PWM
 import time
  
-pwm = PWM(Pin(16))
+pwm = PWM(Pin(17))
  
 pwm.freq(1000)
- 
-duty = 0
-direction = 1
- 
-for _ in range(16*255):
-    duty += direction
- 
-    if duty > 255:
-        duty = 255
-        direction = -1
-    elif duty < 0:
-        duty = 0
-        direction = 1
- 
-    pwm.duty_u16(duty*duty)
-    time.sleep(0.002)
- 
+
+
+while 1:
+    duty = 0
+    direction = 1
+     
+    for _ in range(16*255):
+        duty += direction
+     
+        if duty > 255:
+            duty = 255
+            direction = -1
+        elif duty < 0:
+            duty = 0
+            direction = 1
+     
+        pwm.duty_u16(duty*duty)
+        time.sleep(0.002)
+     
