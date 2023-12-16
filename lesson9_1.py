d01e4000f32216967b85a71ee87c9314854e89de
YunLou <151415345+Yun-Lou-Chen@users.noreply.github.com>
2023-12-16 02:41:14 +0000

    modily

---
+++ lesson9_1.py
@@ -0,0 +1,27 @@
+# PWM驅動LED
+from machine                import Pin,PWM
+import time
+ 
+pwm = PWM(Pin(17))
+ 
+pwm.freq(1000)
+ 
+duty = 0
+direction = 1
+
+def ledloop():
+    for _ in range(16*255):
+        duty += direction
+     
+        if duty > 255:
+            duty = 255
+            direction = -1
+        elif duty < 0:
+            duty = 0
+            direction = 1
+     
+        pwm.duty_u16(duty*duty)
+        time.sleep(0.002)
+ 
+while 1:
+    ledloop()
