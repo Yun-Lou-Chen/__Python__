fac131a7391b5568d0789bf838a2f1aeeaa714fb
YunLou <151415345+Yun-Lou-Chen@users.noreply.github.com>
2024-02-03 11:53:08 +0800

    Add files via upload

---
+++ GUI/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+# df = pd.DataFrame({
+# '第一欄':[1,2,3,4],
+# '第2欄':[10,20,30,40]
+# })
+
+
+
+st.title(':blue[第一欄]:red[第2欄]')
+st.markdown("## [**參考網頁**](http://bestdiy.ru/perevodim-nastennye-chasy-na-pitanie-ot-li-ion-akkumulyatora-na-3-7v.html)")
+st.markdown('---')
+st.markdown("![](http://bestdiy.ru/wp-content/uploads/2021/04/nastennye-chasy-ot-li-ion-akkumulyatora-na-3-7v-01.jpg")
+st.markdown(':-1: :+1: :100: :clock1030: ')
+st.markdown('[**Good idea**](https://www.youtube.com/watch?v=Mv5-garglY0){%youtube Mv5-garglY0 %}')
+
+
